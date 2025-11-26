4591239a660f16932461fce9d275110ae26c9b66
gfpo240276-alt <gfpo240276@upemor.edu.mx>
2025-11-26 17:16:31 -0600

    Implement form submission alert functionality
    Add form submission handling and alert on submit.

--- js/JS.js
+++ js/JS.js
@@ -33,4 +33,14 @@ document.addEventListener('DOMContentLoaded', () => {
         : '¡Bienvenido a este mundo del arte!';
     });
   }
+
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const formulario = document.querySelector("form");
+
+  formulario.addEventListener("submit", function (event) {
+    event.preventDefault(); 
+    alert("Formulario capturado"); 
+    
+  });
 });
